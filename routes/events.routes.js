dff14e9a2f332aaef64d047ad94a8fdd25df777c
Your Name <you@example.com>
2022-10-08 16:25:55 -0300

    cambiando nombre del archivo eventos a calendarEvents

--- routes/events.routes.js
+++ routes/events.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'; 
 import { check } from 'express-validator';
-import { getAllEvents,createEvent,updateEvent,deleteEvent } from '../controllers/events.js'
+import { getAllEvents,createEvent,updateEvent,deleteEvent } from '../controllers/calendarEvents.js'
 import { validarJWT } from '../middlewares/validar-jwt.js';
 import { validateErrors } from '../middlewares/validate-errors.js';
 import { isDate } from '../helpers/isDate.js';
@@ -12,13 +12,13 @@ router.get('/',validarJWT,getAllEvents);
 router.post(
     '/',
     [
-       /*  check('title','Title is required').not().isEmpty(),
+        check('title','Title is required').not().isEmpty(),
         check('title','Title must be at least 3 characters').isLength({ min: 3 }),
         check('note','Note is required').not().isEmpty(),
         check('note','Note must be at least 3 characters').isLength({ min: 3 }),
         check('start','Start date is required').custom(isDate),
         check('end','End date is required').custom(isDate),
-        check('user','User is required').not().isEmpty(), */
+        check('user','User is required').not().isEmpty(),
 
     ],
     validateErrors,
